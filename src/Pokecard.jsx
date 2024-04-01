cc49edd92cd2030f1510c4e04b9e38b77c92aa6b
Max Jeon <maxjeon97@gmail.com>
2024-04-01 16:23:29 -0700

    Add css styling

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,16 +1,18 @@
+import './Pokecard.css';
 
-function Pokecard({id, name, type, exp}) {
-    const pokemonImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+/**Takes in pokemon data as properties, returns Pokecard component */
+function Pokecard({ id, name, type, exp }) {
+    const pokemonImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     return (
         <div className="Pokecard">
-            <div className="card">
+            <div className="Pokecard-card card">
                 <h5 className="Pokecard-title">{name}</h5>
                 <img className="Pokecard-image" src={pokemonImg} alt={name} />
                 <p className="Pokecard-type">Type: {type}</p>
                 <p className="Pokcard-exp"> EXP: {exp}</p>
             </div>
         </div>
-    )
+    );
 
 }
 
